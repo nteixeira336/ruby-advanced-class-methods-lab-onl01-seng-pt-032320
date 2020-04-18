17356afd4a735f000381c984290cff609fd0a0af
Ninibeth Teixeira <ninibethreina@gmail.com>
2020-04-18 20:27:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,6 +32,15 @@ class Song
   def self.find_by_name(name)
     @@all.find{|song| song.name == name }
   end 
+  
+  def self.find_or_create_by_name(name)
+    if song.name != nil 
+      
+    else
+      
+    end 
+  end 
+    
     
 
 end
